03f159b16c3e790fd4b58757876da171d544bc61
Xihai Luo <xihai.luo01@gmail.com>
2021-10-14 17:43:06 -0400

    implemented drawGrid function to draw the inital grids

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ jQuery(function() {
     //create a div inside element to serve as a wrapper
     element.append('<div class="wrapper"></div>');
     //draw a grid
-    drawGrid();
+    drawGrid(data);
   }
 
-  function drawGrid() {
+  function drawGrid(data) {
     //find the max value in dataset and set it as height
-    let height = 5;
+    let height = Math.max(...data);
     //set the width as length of dataset
     let width = data.length;
+    //get the width and height of the rendering element
+    const elmWidth = parseInt(element.css('width')) / width;
+    const elmHeight = parseInt(element.css('height')) / height;
 
     //draw grid
     for (let i = 1; i <= height; i++) {
       for (let j = 1; j <= width; j++) {
-        $('.wrapper').append('<div>hi</div>');
+        $('.wrapper').append('<div class="bar"></div>');
       }
     }
+
+    //set width and height of individual grids
+    $('.bar').css('width', elmWidth);
+    $('.bar').css('height', elmHeight);
   }
 
   //testing
